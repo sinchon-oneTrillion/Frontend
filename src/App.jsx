8fa6db3dc78e2230e7bcd995194efa5040e48d3c
김준희 <shadd3338@gmail.com>
2025-09-05 21:34:12 +0900

    feat/home페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import './App.css'
-
 import {Login} from './pages/Login'
+import {Home} from './pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route>
+          <Route index element={<Home />} />
           <Route path="/login" element={<Login/>}/>
         </Route>
       </Routes>
